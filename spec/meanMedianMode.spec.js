8a50930b401e101f08971c6f4feaf2c8fa955c7b
Kamal Joshi <kamal2311@gmail.com>
2018-03-19 21:40:57 -0400

    Added mode function.

--- spec/meanMedianMode.spec.js
+++ spec/meanMedianMode.spec.js
@@ -9,7 +9,7 @@ describe("Mean median mode", () => {
         });
 
         it("should throw an error if no argument supplied", () => {
-            expect(() => { mean() }).toThrow(new Error('Must pass an array'));
+            expect(() => { mean() }).toThrow(new Error('InvalidInput'));
         });
 
         it('should return 0 if the array is empty', () => {
@@ -25,5 +25,34 @@ describe("Mean median mode", () => {
         it('should return 3 as the median of [1,2,3,7,8]', () => {
             expect(median([1, 2, 3, 7, 8])).toBe(3);
         });
+    });
+
+    describe('Mode', () => {
+        it('should return 4 as the mode of [1,2,3,3,4,4,4,7]', () => {
+            expect(mode([1, 2, 3, 3, 4, 4, 4, 7])).toBe(4);
+        });
+
+        it('should return 5 as the mode of [1,2,13,4,5]', () => {
+            expect(mode([1, 2, 13, 4, 5])).toBe(5);
+        });
+
+        it('should throw InvalidInput error if no argument supplied', () => {
+            expect(() => { mode() }).toThrow(new Error("InvalidInput"));
+        });
+    });
+
+    it("should return mean, median and mode of an array", () => {
+
+        const answer = meanMedianMode([1, 2, 3, 7, 4, 4, 4, 6, 9, 10]);
+        const expectedMean = 5;
+        const expectedMedian = 4;
+        const expectedMode = 4;
+
+        expect(answer.mean).toBe(expectedMean);
+    });
+
+    it('should throw InvalidInput error if no argument', () => {
+
+        expect(() => { meanMedianMode() }).toThrow(new Error("InvalidInput"));
     });
 });
--- src/meanMedianMode.js
+++ src/meanMedianMode.js
@@ -1,7 +1,7 @@
 function mean(inputArray) {
 
     if (inputArray === undefined) {
-        throw new Error('Must pass an array');
+        throw new Error('InvalidInput');
     }
 
     if (inputArray.length === 0) {
@@ -26,3 +26,39 @@ function median(inputArray) {
     return (inputArray[length / 2] + inputArray[length / 2 - 1]) / 2;
 }
 
+function mode(inputArray) {
+
+    if (inputArray === undefined) {
+        throw new Error("InvalidInput");
+    }
+
+    const counts = {};
+    let maxOccuringVal = inputArray[0];
+    let maxFrequency = 1;
+    inputArray.forEach(val => {
+        if (counts.hasOwnProperty(val)) {
+            counts[val]++;
+        }
+        else {
+            counts[val] = 1;
+        }
+
+        if (counts[val] >= maxFrequency) {
+            maxFrequency = counts[val];
+            maxOccuringVal = val;
+        }
+    });
+
+    return maxOccuringVal;
+}
+
+
+function meanMedianMode(inputArray) {
+    const answer = {};
+
+    answer.mean = mean(inputArray);
+    answer.median = median(inputArray);
+    answer.mode = mode(inputArray);
+
+    return answer;
+}
